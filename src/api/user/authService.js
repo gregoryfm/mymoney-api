2d3d36b51e2be49109e592ad87142fab5e316980
Gregory Menezes <gregoryfontenele@gmail.com>
2018-06-05 19:05:32 -0300

    login and validateToken methods

--- src/api/user/authService.js
+++ src/api/user/authService.js
@@ -12,3 +12,29 @@ const sendErrosFromDB = (res, dbErrors) => {
     _.forIn(dbErrors.errors, error => errors.push(error.message))
     return res.status(400).json({ errors });
 }
+
+const login = (request, response, next) => {
+    const email = request.body.email || '';
+    const password = request.body.password || '';
+
+    User.findOne({ email }, (err, user) => {
+        if (err) {
+            return sendErrosFromDB(response, err);
+        } else if ( user && bcrypt.compareSync(password, user.password)) {
+            const token = jwt.sign(user, env.authSecret, {
+                expiresIn: '1 day'
+            })
+            const { name, email } = user;
+            response.json({ name, email, token })
+        } else {
+            return response.status(400).send({ errors: ['Usuário/Senha inválidos'] })
+        }
+    });
+}
+
+const validateToken = (request, response, next) => {
+    const token = request.body.token || '';
+    jwt.verify(token, env.authSecret, function (err, decoded) {
+        return response.status(200).send({ valid: !err });
+    })
+}
